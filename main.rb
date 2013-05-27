becb541f2cb1ea06f0f07b88ac7be951fb313e14
Matthew Bunday <zenceph@gmail.com>
2013-05-27 18:06:19 -0400

    separating logic into files.

--- main.rb
+++ main.rb
@@ -6,6 +6,9 @@ require 'haml'
 require 'sinatra'
 require 'mongo'
 
+require_relative './prosedy/draft'
+require_relative './prosedy/user'
+
 enable :sessions
 
 $client = Mongo::MongoClient.new('localhost', 27017)
@@ -13,45 +16,6 @@ $db = $client.db('storiesarealive')
 $users = $db.collection('users')
 $drafts = $db.collection('drafts')
 DATA_DIR = "./data"
-
-class Draft
-    attr_accessor :content, :draft
-
-    def initialize(user, draft, content)
-        @user, @draft, @content = user, draft, content
-        @dir = "#{DATA_DIR}/#{@user}/#{@draft}"
-    end
-
-    def create
-        if $drafts.find_one({:user => @user, :draft => @draft})
-            raise 'Draft already exists'
-        end
-
-        `mkdir #{@dir}`
-        `git init #{@dir}`
-
-        draft_id = $users.find_and_modify(:query => {:user => @user}, :update => {"$inc" => {"draft_count" => 1}}, :new => true)['draft_count']
-        $drafts.insert({:user => @user, :draft => @draft, :dir => @dir, :num => draft_id})
-
-        File.open("#{@dir}/draft.textile", 'w') do |f|
-            f.puts @content
-        end
-
-        `git --git-dir=#{@dir}/.git --work-tree=#{@dir} add draft.textile`
-        `git --git-dir=#{@dir}/.git --work-tree=#{@dir} commit -a -m 'initial commit'`
-    end
-
-    def self.get_draft(user, num)
-        draft = $drafts.find_one({:user => user, :num => num})
-        return nil if draft.nil? 
-        draft['content'] = File.open("#{draft['dir']}/draft.textile").read
-        return Draft.new(user, draft['draft'], draft['content'])
-    end
-
-    def self.get_drafts(user)
-        $drafts.find({:user => user}).to_a
-    end
-end
 
 helpers do
     def logged_in?
@@ -123,6 +87,7 @@ post "/signup" do
     session[:username] = username
 
     # create the directory where we will store git repos for the user
+    `mkdir #{DATA_DIR}`
     `mkdir #{DATA_DIR}/#{username}`
 
     redirect "/"
