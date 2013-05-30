1329c8853b90aa35818dae6f15555eeb961334f6
Matthew Bunday <zenceph@gmail.com>
2013-05-30 00:25:59 -0400

    finish up writer class.

--- prosedy/writer.rb
+++ prosedy/writer.rb
@@ -1,10 +1,18 @@
 require 'rubygems'
 require 'bcrypt'
 
+Writer = Struct.new :_id, :dc
+
 class WriterManager
     def initialize(prosedy)
         @prosedy = prosedy
         @writer_db = @prosedy.db.collection('writers')
+    end
+
+    def h_to_st(writer)
+        w = Writer.new
+        writer.each { |k,v| w[k] = v }
+        return w
     end
 
     def create(name, password)
@@ -16,16 +24,21 @@ class WriterManager
         writer = {_id: name, ph: ph, ps: ps, dc: 0}
         @writer_db.insert(writer)
         
-        return writer
+        return h_to_st(writer)
     end
 
     def login(name, password)
         writer = find_by_name(name)
-        (writer && writer.ph == BCrypt::Engine.hash_secret(password, writer.ps)) ? writer : nil
+        (writer && writer.ph == BCrypt::Engine.hash_secret(password, writer.ps)) ? h_to_st(writer) : nil
     end
 
     def inc_draft_c(name)
         @writer_db.find_and_modify(query: {_id: name}, update: {'$inc' => {dc: 1}}, fields: ['dc'], new: true)['dc']
+    end
+
+    def find_by_name(name)
+        writer = @writer_db.find_one({_id: name}, fields: ['_id', 'dc'])
+        return writer ? h_to_st(writer) : nil
     end
 end
 
