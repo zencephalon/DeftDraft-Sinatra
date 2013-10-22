9f2f24224371b3926466cc51666d96e33348a00c
Matthew Bunday <zenceph@gmail.com>
2013-10-21 23:01:25 -0400

    Working on posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,16 @@
 class PostsController < ApplicationController
   def new
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
