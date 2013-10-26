f793fd471b0ce47132299b83cf540500adc1ba45
Matthew Bunday <zenceph@gmail.com>
2013-10-26 15:20:31 -0400

    DRYing up views, adding links, add edit action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,30 @@
 class PostsController < ApplicationController
   def new
+      @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
 
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def show
